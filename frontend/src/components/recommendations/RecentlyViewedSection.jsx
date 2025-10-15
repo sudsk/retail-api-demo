40ed36729c920cb2ceef6631ec16ee30d28e6edd
Suds Kumar <suds123@gmail.com>
2025-10-15 14:57:07 +0100

    Create RecentlyViewedSection.jsx

---
+++ frontend/src/components/recommendations/RecentlyViewedSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecommendedProducts from './RecommendedProducts';
+
+const RecentlyViewedSection = () => {
+  return (
+    <RecommendedProducts
+      model="recently_viewed"
+      title="Recently Viewed"
+    />
+  );
+};
+
+export default RecentlyViewedSection;
