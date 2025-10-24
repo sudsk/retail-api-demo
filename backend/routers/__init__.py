d3f3d95a047ce810fb012441dc23c906a089057c
Suds Kumar <suds123@gmail.com>
2025-10-24 10:04:52 +0100

    Update __init__.py

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,5 +1,6 @@
 from .search import router as search_router
 from .products import router as products_router
 from .recommendations import router as recommendations_router
+from .categories import router as categories_router
 
-__all__ = ["search_router", "products_router", "recommendations_router"]
+__all__ = ["search_router", "products_router", "recommendations_router", "categories_router"]
